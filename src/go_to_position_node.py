b7aa863449491473127cc13e521de7d68c2775e9
Zach417 <zach@slaterobots.com>
2018-08-23 08:49:07 -0500

    left arm testing

--- src/go_to_position_node.py
+++ src/go_to_position_node.py
@@ -22,7 +22,7 @@ display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path
 
 robot = moveit_commander.RobotCommander()
 scene = moveit_commander.PlanningSceneInterface()
-group = moveit_commander.MoveGroupCommander("right_arm")
+group = moveit_commander.MoveGroupCommander("left_arm")
 
 table = geometry_msgs.msg.PoseStamped()
 table.header.frame_id = robot.get_planning_frame()
